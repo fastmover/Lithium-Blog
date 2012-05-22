222ca8e3b1459d8a89e2751026884fa5dd090377
Steve <Fastmoverr@gmail.com>
2012-05-22 15:23:25 -0500

    Session Support, Unfinished patch

--- app/config/bootstrap/session.php
+++ app/config/bootstrap/session.php
@@ -37,12 +37,12 @@ Session::config(array(
  */
 // use lithium\security\Auth;
 
-// Auth::config(array(
-// 	'default' => array(
-// 		'adapter' => 'Form',
-// 		'model' => 'Users',
-// 		'fields' => array('username', 'password')
-// 	)
-// ));
+Auth::config(array(
+	'default' => array(
+ 		'adapter' => 'Form',
+ 		'model' => 'Users',
+ 		'fields' => array('username', 'password')
+ 	)
+));
 
 ?>
