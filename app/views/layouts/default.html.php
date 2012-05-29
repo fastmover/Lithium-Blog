344510316056cea309468ebff6cc82d37abd9fb7
Steve <Fastmoverr@gmail.com>
2012-05-29 14:12:11 -0500

    Menu Added to template

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -22,69 +22,9 @@
 	
 </head>
 <body class="app">
-<div class="navbar navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
-			<a class="brand" href="#">Project name</a>
-			<div class="nav-collapse">
-				<ul class="nav">
-					<li class="active"><a href="#">Home</a></li>
-					<li><a href="#">Link</a></li>
-					<li><a href="#">Link</a></li>
-					<li><a href="#">Link</a></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li class="nav-header">Nav header</li>
-							<li><a href="#">Separated link</a></li>
-							<li><a href="#">One more separated link</a></li>
-						</ul>
-					</li>
-				</ul>
-				<form class="navbar-search pull-left" action="">
-					<input type="text" class="search-query span2" placeholder="Search">
-				</form>
-				<ul class="nav pull-right">
-					<li><a href="#">Link</a></li>
-					<li class="divider-vertical"></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php 
-							if($this->login->userName()) {
-								echo $this->login->userName();
-							} else {
-								echo "Login";
-							}
-						?><b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<?php
-								if(!$this->login->userName()) {
-									?><li><?=$this->html->link("Login", array('Users::login')); ?></li><?php
-								} else {
-									?><li><?=$this->html->link("Logout", array('Users::logout')); ?></li><?php
-								}
-								/*
-							?>
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li><a href="#">Separated link</a></li><?php // */ ?>
-						</ul>
-					</li>
-				</ul>
-			</div><!-- /.nav-collapse -->
-		</div>
-	</div><!-- /navbar-inner -->
-</div>
+
+<?= $this->_render("element", "menu", array("asdf" => "base" )) ?>
+
 	<div class="container">
 		<div id="margintopforty">
 			<div class="span12">
